db77cf135f2fa1e0f4919bd79e77f296c7525510
Simtimus <simtimusbos@gmail.com>
2023-04-22 12:39:49 +0300

    Added ignored files

---
+++ SmartHome.Arduino/Models/Logs/ClassLog.cs
@@ -0,0 +1,26 @@
+﻿using SmartHome.Arduino.Application.Logging;
+using SmartHome.Arduino.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Arduino.Models.Logs
+{
+    public class ClassLog<TClass> : ILog
+    {
+        public DateTime Time { get; set; } = DateTime.Now;
+        public string Message { get; set; } = string.Empty;
+        public LoggingService.LogStates LogState { get; set; }
+        public TClass Class { get; set; }
+        public string CodeSpace { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"{Enum.GetName(typeof(LoggingService.LogStates), LogState)} : [{CodeSpace}]\n" +
+                $"\t{Message}\n" +
+                $"\tWhen [{nameof(TClass)}] is \"{Class?.ToString()}\"";
+        }
+    }
+}
---
+++ SmartHome.Arduino/Models/Logs/ParametreLog.cs
@@ -0,0 +1,27 @@
+﻿using SmartHome.Arduino.Application.Logging;
+using SmartHome.Arduino.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Arduino.Models.Logs
+{
+    public class ParametreLog<TClass, TValue> : ILog
+    {
+        public DateTime Time { get; set; } = DateTime.Now;
+        public string Message { get; set; } = string.Empty;
+        public LoggingService.LogStates LogState { get; set; }
+        public TValue Value { get; set; }
+        public string Propriety { get; set; } = string.Empty;
+        public string CodeSpace { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"{Enum.GetName(typeof(LoggingService.LogStates), LogState)} : [{CodeSpace}]\n" +
+                $"\t{Message}\n" +
+                $"\tWhen [{nameof(TClass)}.{typeof(TValue)}] {Propriety} = \"{Value}\"";
+        }
+    }
+}
